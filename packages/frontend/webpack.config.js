902b15259d36e9854721fc79fb4b410e1c8b1061
Amarbir Singh <amarbir22@gmail.com>
2022-08-18 11:39:02 -0400

    chore: add bundle analyzer

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
     entry: './src/index.tsx',
     output: {
@@ -33,6 +35,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, '/src/index.html')
-        })
+        }),
+        new BundleAnalyzerPlugin({analyzerMode: 'server'})
     ]
 }
